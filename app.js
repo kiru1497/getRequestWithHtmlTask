325e7f6e2f1f31338b86bad72a25fe47d7980c8b
Kiran R <kiran.r1497@gmail.com>
2025-12-30 12:01:06 +0530

    postrequestwithhtml

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require("express"); 
-const path = require("path"); 
-const app = express(); 
+const express = require("express");
+const path = require("path");
 
-const port = 3000; 
-
-
-app.get("/api/product",(req,res)=>{
-    res.sendFile(path.join(__dirname,"views","products.html")); 
-})
+const app = express();
+const port = 3000;
 
 
-app.listen(port,()=>{
-    console.log("Server is running."); 
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/api/product", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "products.html"));
+});
+
+
+app.post("/api/product", (req, res) => {
+  const productName = req.body.productName;
+
+  console.log("Product received:", productName);
+
+  res.send(`Product added: ${productName}`);
+});
+
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
